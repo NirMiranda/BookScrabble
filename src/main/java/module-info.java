9f8a66bf8375a1a58a4f7b3950778aa9cfb2cc28
ori farkash <109721584+orifarkash@users.noreply.github.com>
2023-05-02 13:13:37 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.newproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.newproject to javafx.fxml;
-    exports com.example.newproject;
-}
